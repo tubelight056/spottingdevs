a3f484a5df6463ea421a0fc184b9897553d8dae8
tubelight056 <69566026+tubelight056@users.noreply.github.com>
2022-03-09 14:14:37 +0530

    added search by get by skills

---
+++ collections/getParticularSkilled.js
@@ -0,0 +1,66 @@
+const { User } = require("../models/User");
+
+exports.getParticularlySkilled = (req, res) => {
+  console.log("\n[+]  request", req.method, req.originalUrl);
+  console.log("[+] ", req.body);
+  try {
+    if (req.body.skills === undefined || req.body.skills === []) {
+      console.log(`[-]  `, {
+        statusMessage: "Atleast a skill is required ",
+        status: false,
+      });
+      res.send({
+        statusMessage: "Atleast a skill is required ",
+        status: false,
+      });
+    }
+    await User.find(
+      { Skills: { $in: [...req.body.skills] } },
+      async (err, result) => {
+        if (err) {
+          console.log(`[-]  `, {
+            error: err,
+            statusMessage: "something went wrong",
+            status: false,
+          });
+          res.send({
+            error: err,
+            statusMessage: "something went wrong",
+            status: false,
+          });
+        } else {
+          if (result !== []) {
+            console.log({
+              data: result,
+              status: true,
+            });
+            res.send({
+              data: result,
+              status: true,
+            });
+          } else {
+            console.log(`[-]  `, {
+              statusMessage: "No Users founded",
+              status: false,
+            });
+            res.send({
+              statusMessage: "no  Users founded",
+              status: false,
+            });
+          }
+        }
+      }
+    ).clone();
+  } catch (err) {
+    console.log(`[-]  `, {
+      error: err,
+      statusMessage: "something went wrong",
+      status: false,
+    });
+    res.send({
+      error: err,
+      statusMessage: "something went wrong",
+      status: false,
+    });
+  }
+};
--- routers/getDetails.js
+++ routers/getDetails.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const { getDetails } = require("../collections/getDetails");
+const {
+  getParticularlySkilled,
+} = require("../collections/getParticularSkilled");
 const { verifyToken } = require("../collections/middleware/verifyToken");
 const Router = express.Router();
 
 Router.post("/alldetails", verifyToken, getDetails);
+Router.post("/user/skills", verifyToken, getParticularlySkilled);
 
 module.exports = Router;
